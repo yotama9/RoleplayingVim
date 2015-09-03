b2640ec7d239cf1f28262a3bfeb370a3f76108ee
Yotam <yotama9@gmail.com>
2015-09-03 23:09:53 +0300

    The die roller now supports section names. This is also added to the documentation

--- autoload/RPGC/die_roller.py
+++ autoload/RPGC/die_roller.py
@@ -1,3 +1,4 @@
+import re
 import vim
 from numpy.random import randint
 
@@ -8,86 +9,143 @@ def print_error():
         1d6
         1d20+1
         3d6-2
-    Currently, only one die type is possible
+    Currently, only one die type is supported
     """
 
-def roll_die(die):
-    n,dtype,bonus = die
-    out = 0
-    i = 0
-    while n > i:
-        v = randint(1,dtype+1)
-        out += v
-        print '{}) result: {}, total: {}'.format(i,v,out)
-        i += 1
 
-    print '-'*5
-    print 'total: {}'.format(out+bonus)
+def roll_dice(roll_data):
+    total = 0
+    bonus = die_data['mod']
+    for key in ['add','sub']:
+        for i in range (len(die_data[key])):
+            if not 'd' in die_data[key][i]: continue
+            #All the fixed bonuses are placed in 'mod'
+                
+            n,d = die_data[key][i].strip().split('d')
+            if n == '':
+                n = 1
+            else:
+                n = int(n)
+            d = int(d)
+            for i in range (n):
+                print 'd{}: '.format(d),
+                res = randint(0,d+1)
+                total += res
+                print 'result={}, total={}'.format(res,total)
+    print '-'*10
+    print 'Total+bonus={}'.format(total+bonus)
+            
+    return
     
 
-def die_maker(die=None):
-    #A function to split die into number - die type - bonus
-    
-    #if passed die is none, it is currently yanked
-    if die == None:
-        die = vim.eval('@"')
-    try: 
-        n,rest = die.split('d')
-    except ValueError: 
-        print "More than one d, can't be handled yet"
-        return False
-    if n.strip() == '':
-        n = '1'
-    #Generate bonus
-    if '-' in rest:
-        dtype,bonus = rest.split('-')
-        bonus = '-'+bonus
-    elif '+' in rest:
-        dtype,bonus = rest.split('+')
-    else:
-        dtype,bonus = rest,0
-    
-    try:
-        return [int(v) for v in n,dtype,bonus]
-    except ValueError:
-        return False
+def get_bonus(die_string):
 
-def get_die_parts_by_name():
+    """ A function to split a string based on + and - signs
+    returns a dict with 'add' and 'sub' keys"""
+
+    out = {'add':[],'sub':[],'mod':0}
+    
+    target = 'add'
+    while '+' in die_string or '-' in die_string:
+        nextp = die_string.find('+')
+        nextm = die_string.find('-')
+
+        if nextp < nextm and nextp > 0 or nextm < 0:
+            to_put,die_string = die_string.split('+',1)
+            out[target].append(to_put)
+            target = 'add'
+
+        else:
+            to_put,die_string = die_string.split('-',1)
+            out[target].append(to_put)
+            target = 'sub'
+
+    out[target].append(die_string)        
+    return out
+
+def die_from_text(die_text):
+    """ A function to check if a certain text is a die, a bonus, or a predefined
+    die. If the latter is the case, the function would return the die
+    definition"""
+
+    #Checking if this follows a regular die notation or a simple bonus
+    if re.match("[0-9]*d[0-9]*",die_text):
+        return die_text
+    elif re.match("[0-9]+",die_text):
+        return die_text
+    #Not a die, searching for the die definition
+    lnum = 0
+    n0 = 0
+    pre = ''
+    while '.' in die_text:
+        first,die_text = die_text.split('.',1)
+        n0 += 1
+        pre = '*'*n0 
+        pattern = '^' + '\*'*n0
+
+        while lnum < len(vim.current.buffer):
+            line = vim.current.buffer[lnum]
+            if re.match(pattern,line) and first in line:
+                lnum += 1
+                break 
+            lnum += 1
+        
+
+        
+    for line in vim.current.buffer[lnum:]:
+        print lnum
+        if die_text in line and '=' in line:
+            die_info=line.split('=')[1]
+            #definition found, return it
+            return die_info
+        if re.match(pattern,line):
+            break
+    
+
+
+
+def die_converter():
     die = vim.eval('@"')
 
     #split bonus from main die
-    if '+' in die:
-        die,bonus_text = die.split('+')
-        bonus_text = '+{}'.format(bonus_text)
-        bonus = int(bonus_text)
-    elif '-' in die:
-        die,bonus = die.split('-')
-        bonus_text = '-{}'.format(bonus_text)
-        bonus = -int(bonus_text)
+    die_parts = get_bonus(die)
+    
+    #replace predefined dies and bonuses with die notation
+    for key in ['add','sub']:
+        for i in range (len(die_parts[key])):
+            die_data = die_from_text(die_parts[key][i])
+            try: #Checking if this is a simple numerical value
+                #yes add it
+                die_data = int(die_data)
+                if key == 'add':
+                    die_parts['mod'] += die_data
+                else:
+                    die_parts['mod'] -= die_data
+            except ValueError:
+                #no, place it in the appropriate list
+                die_parts[key][i] = die_data
+                pass
 
-    #search for the die definition in the file
-    for line in vim.current.buffer[:]:
-        if die in line and '=' in line:
-            die_info=line.split('=')[1]
-            n,dtype,trm = die_maker(die_info)
+    print die_parts
+    return die_parts
+    
 
-    print "rolling {}d{}{}".format(n,dtype,bonus_text)
-    return [n,dtype,bonus]
+    
+
         
 
         
 
 
 #Get die data, retrun False if die infor is faulty
-
-#first try by names
-die_data = get_die_parts_by_name()
-#if this fails, try by die notation
+die_data = die_converter()
 if not die_data:
-    die_data = die_maker()
-
-
-if die_data == False:
     print_error()
 else:
-    roll_die(die_data)
+    roll_dice(die_data)
+
+
+#if die_data == False:
+#    print_error()
+#else:
+#    roll_die(die_data)
